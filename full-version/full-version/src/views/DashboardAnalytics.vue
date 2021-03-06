e030184e6ae18599de101df39f87d67d474080e5
Enrico <forty5degreesdev@gmail.com>
2021-03-06 12:46:23 +0200

    Added my name to project

--- full-version/full-version/src/views/DashboardAnalytics.vue
+++ full-version/full-version/src/views/DashboardAnalytics.vue
@@ -17,7 +17,8 @@
                     <img src="@/assets/images/elements/decore-left.png" class="decore-left" alt="Decore Left" width="200" >
                     <img src="@/assets/images/elements/decore-right.png" class="decore-right" alt="Decore Right" width="175">
           <feather-icon icon="AwardIcon" class="p-6 mb-8 bg-primary inline-flex rounded-full text-white shadow" svgClasses="h-8 w-8"></feather-icon>
-          <h1 class="mb-6 text-white">Congratulations {{ checkpointReward.userName }},</h1>
+          <!-- <h1 class="mb-6 text-white">Congratulations {{ checkpointReward.userName }},</h1> -->
+          <h1 class="mb-6 text-white">Congratulations {{ enricodata.name }}</h1>
           <p class="xl:w-3/4 lg:w-4/5 md:w-2/3 w-4/5 mx-auto text-white">You have done <strong>{{ checkpointReward.progress }}</strong> more sales today. Check your new badge in your profile.</p>
         </vx-card>
       </div>
@@ -250,6 +251,7 @@ export default {
       supportTracker: {},
       productsOrder: {},
       salesRadar: {},
+      enricodata: {},
 
       timelineData: [
         {
@@ -315,6 +317,8 @@ export default {
 
     var self = this;
     self.$store.dispatch("updateTheme", "dark");
+
+    self.enricodata.name = 'Enrico Dreyer';
 
     //  User Reward Card
     this.$http.get('/api/user/checkpoint-reward')
