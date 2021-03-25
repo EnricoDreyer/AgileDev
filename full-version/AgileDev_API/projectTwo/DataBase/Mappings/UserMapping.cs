2a05b8d352dba25161b4183b3e53a0df2e660f96
Enrico <forty5degreesdev@gmail.com>
2021-03-25 08:20:41 +0200

    Added mappings

---
+++ full-version/AgileDev_API/projectTwo/DataBase/Mappings/UserMapping.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using projectTwo.Models;
+
+
+namespace projectTwo.DataBase.Mappings
+{
+	public class UserEntityConfiguration : EntityCongurationMapper<User>
+	{
+		public override void Configure(EntityTypeBuilder<User> builder)
+		{
+			builder.HasKey(c => c.Id);
+
+			builder.ToTable("User");
+		}
+	}
+}
